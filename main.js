bd4b852d3e43821c98b3c47142d5c693dc9e71f9
Hamsa Sarees <hamsasarees@gmail.com>
2026-02-01 04:24:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+window.addEventListener("DOMContentLoaded", () => {
+  window.TIGEN = {};
+  TIGEN.scene = new TIGEN_Scene();
+  TIGEN.loop = new TIGEN_Loop(TIGEN.scene);
+  TIGEN.loop.start();
+});
 
